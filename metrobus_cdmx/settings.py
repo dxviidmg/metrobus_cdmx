22bcc257d8ab776c489508cc86c3819f51bc20ba
David <david@ixulabs.com>
2022-03-07 14:05:22 -0600

    database and req

--- metrobus_cdmx/settings.py
+++ metrobus_cdmx/settings.py
@@ -83,10 +83,14 @@ WSGI_APPLICATION = 'metrobus_cdmx.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+	'default': {
+    	'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    	'NAME': 'metrobus_cdmx',
+    	'USER': 'postgres',
+    	'PASSWORD': 'postgres',
+    	'HOST': 'localhost',
+    	'PORT': '5432',
+	}
 }
 
 
